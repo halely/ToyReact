8a00456770ab4ba202bf66f00c8825fac2fe4f8a
halely <1060610431@qq.com>
2020-07-28 23:24:46 +0800

    第一天

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
-import {ToyReact} from './ToyReact.js';
-
-class Mycomponent {
-	
+import {ToyReact,Component} from './ToyReact.js';
+class Mycomponent extends Component{
+	render(){
+		return <div>
+		<span>Hello</span>
+		<span>world</span>
+		<div>
+		{true}
+		{this.children}
+		</div>
+		</div>
+	}
 }
-let a= <Mycomponent name="a" />
+
+let a=<Mycomponent name="a" id="ida">
+<div>123</div>
+</Mycomponent>
+
+ToyReact.render(
+    a,
+	document.body
+)
+	
